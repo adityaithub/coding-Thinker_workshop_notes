f2eed4f1a5b98db6c07786977858e254cb54bd41
Aditya Sharma <adityasharma722001@gmail.com>
2024-08-21 17:08:05 +0530

    Day-1 completed Learning about the basics

---
+++ Day-1/app.js
@@ -0,0 +1,102 @@
+// Variables
+// let x = "Rahul";
+// x="riya";
+// console.log(x);
+
+// Datatypes
+/**
+ * Number
+ * String
+ * boolean
+ * undefined
+ * null
+ * BigInt
+ * Symbol
+ */
+
+let a = 12.123464;  //Number
+let name = "Rahul"; // String
+let isSelected = true;
+let y;
+console.log(y); // Undefined
+let cardNumber = null; // Null
+console.log(cardNumber);
+
+// Non Primitive
+/**
+ * Array
+ * Object
+ */
+
+// let arr = [12,13,"Rohan","Anil",true] // Array
+// console.log(arr[2]);
+
+// Objects
+
+// let human = {
+//     name: "Shivam",
+//     phone: 41236321,
+// };
+
+// console.log(human.name);
+// console.log(human.phone);
+
+let val = 10;
+val%=4;
+console.log(val);
+
+
+
+
+//If else
+
+// let age=24;
+
+// if(age >= 18){
+//     console.log("Eligible for Driving");
+    
+// }else{
+//     console.log("Not eligible for driving");
+    
+// }
+
+
+// Nested If else
+
+let age=17;
+let naam = "sahil";
+if(age >= 18){
+    if(naam==="rahul"){
+        console.log("Congrats");
+    }else{
+        console.log("Please change your name");    
+    }
+}else{
+    console.log("Not eligible for driving"); 
+}
+
+// let num =0;
+for(let i=0;i<5;i++){
+    console.log("Number is ",i);
+    
+}
+
+let c = "rahul";
+let d = 24;
+
+console.log("My name is "+c+"and my age is "+d);
+console.log(`My name is ${c} and my age is ${d}`);
+
+
+
+// do {
+//     // code
+// }while(condition);
+
+
+
+
+
+
+
+
