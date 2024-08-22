b5fe4f5d6f2941a11ded1778fb64b611ba00a768
Aditya Sharma <adityasharma722001@gmail.com>
2024-08-22 17:22:54 +0530

    Project-2 background changer completed

--- Day-2/Pro-2/app.js
+++ Day-2/Pro-2/app.js
@@ -1,7 +1,11 @@
 console.log("Please change your background")
-document.querySelector("#btn").addEventListener("click",function(){
-    console.log("Please complete the project");
-    
-})
+document.querySelector("#btn").addEventListener("click",setInterval(
+    function(){
+        console.log("Please complete the project");
+        let colorName = "#"+Math.floor(Math.random()*10000000).toString(16);
+        // console.log(colorName);
+        document.body.style.backgroundColor = colorName; 
+    },1000
+))
 
-document.body.style.backgroundColor = "#23e5a4"; 
+ 
