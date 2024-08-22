fe5675720a1bc53dd007f0d828f5f0e0bbba6834
Aditya Sharma <adityasharma722001@gmail.com>
2024-08-22 16:58:01 +0530

    Project -2 Background ccolor updated

---
+++ Day-2/Pro-2/app.js
@@ -0,0 +1,7 @@
+console.log("Please change your background")
+document.querySelector("#btn").addEventListener("click",function(){
+    console.log("Please complete the project");
+    
+})
+
+document.body.style.backgroundColor = "#23e5a4"; 
