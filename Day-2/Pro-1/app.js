7a2695076b3b5158ae60a9505a6d7900c6813314
Aditya Sharma <adityasharma722001@gmail.com>
2024-08-22 16:40:41 +0530

    Project-1 completed of javascript where you learn how to get the values

--- Day-2/Pro-1/app.js
+++ Day-2/Pro-1/app.js
@@ -1,10 +1,17 @@
 console.log("I am working")
-document.body.style.backgroundColor="red";
+document.body.style.backgroundColor="grey";
 document.querySelector("#userDetail").addEventListener(
     "submit",function(e){
         e.preventDefault();
         let firstName = document.querySelector("#firstName").value
-        console.log(firstName);
+        let lastName = document.querySelector("#lastName").value
+        let phone = document.querySelector("#phone").value
+        let gender = document.querySelector(".genderOption").value
+        let country = document.querySelector(".countrySelect").value
+
+        console.log(firstName,lastName,phone,gender,country);
+
+        
         
     }
     // let firstName = document.querySelector("#firstName")
